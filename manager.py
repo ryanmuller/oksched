501e3bcf3e3c01fc7d7e29a45c2eee69ac087de5
Ryan Muller <cognitionmachine@gmail.com>
2015-03-26 14:10:26 -0400

    clean up manager

--- manager.py
+++ manager.py
@@ -4,16 +4,8 @@ from app import app, init_db
 manager = Manager(app)
 
 @manager.command
-def hello():
-    print "hello"
-
-@manager.command
 def init():
     init_db()
-
-@manager.command
-def seed():
-    print "hello"
 
 if __name__ == "__main__":
     manager.run()
